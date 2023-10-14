4d3d96bab67b2431c7ce95b8437898ef382401de
mongy <=>
2023-10-14 18:50:46 +0300

    react proj

---
+++ react-project/src/App.js
@@ -0,0 +1,68 @@
+import "./App.css"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import "./componants/css/course.css"
+import "./componants/css/Nav-Bar-Styless.css"
+import "./componants/css/Header-Styless.css"
+import "./componants/css/Popover-Styless.css"
+import "./componants/css/Catigoreis.css"
+import "./single-course-page/css/Requires-Styless.css"
+import "./single-course-page/css/header.css"
+import "./componants/css/footer.css"
+import "./single-course-page/css/sidebar.css"
+import NavBar from "./componants/NavBar"
+import Home from "./componants/Home"
+import "./single-course-page/css/maincontent.css"
+import CourseSinglePage from "./single-course-page/CourseSinglePage"
+import "./single-course-page/css/Description-Styless.css"
+import { useEffect, useState } from "react"
+import React from "react"
+import Footer from "./componants/Footer"
+import axios from "axios"
+
+function App() {
+	const [loaded, setLoaded] = useState(false)
+	useEffect(() => {
+		axios
+			.get("http://localhost:3000/data")
+			.then((response) => {
+				var homepage = response.data
+				localStorage.setItem("homepage", JSON.stringify(homepage))
+			})
+			.then(() => {
+				setLoaded(true)
+			})
+
+		axios
+			.get("http://localhost:3000/reviews")
+			.then((response) => {
+				var reviews = response.data
+				localStorage.setItem("reviews", JSON.stringify(reviews))
+			})
+			.then(() => {
+				setLoaded(true)
+			})
+	}, [])
+
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<NavBar />
+				<Routes>
+					<Route exact path="/" element={<Home loaded={loaded} />} />
+					<Route
+						exact
+						path="/course/:courseID"
+						element={<CourseSinglePage />}
+					/>
+					<Route
+						exact
+						path="/search"
+						element={<Home loaded={loaded} />}
+					/>
+				</Routes>
+				<Footer />
+			</div>
+		</BrowserRouter>
+	)
+}
+export default App
